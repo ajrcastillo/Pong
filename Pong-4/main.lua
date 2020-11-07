b47c7cde23c6ed38155fba3fa29ecbb73ab49632
ajrcastillo <72683014+ajrcastillo@users.noreply.github.com>
2020-11-08 01:28:20 +0800

    Add files via upload

---
+++ Pong-4/main.lua
@@ -0,0 +1,96 @@
+push = require 'push'
+
+WINDOW_WIDTH = 1280
+WINDOW_HEIGHT = 720
+
+VIRTUAL_WIDTH = 432
+VIRTUAL_HEIGHT = 243
+
+PADDLE_SPEED = 200
+
+function love.load()
+    love.graphics.setDefaultFilter('nearest', 'nearest')
+
+    math.randomseed(os.time())
+
+    smallFont = love.graphics.newFont('Pixellari.ttf', 8)
+
+
+
+    push:setupScreen(VIRTUAL_WIDTH, VIRTUAL_HEIGHT, WINDOW_WIDTH, WINDOW_HEIGHT, {
+        fullscreen = false,
+        resizable = false,
+        vsync = true
+    })
+
+    player1Y = 30
+    player2Y = VIRTUAL_HEIGHT - 50
+
+    ballX = VIRTUAL_WIDTH / 2 - 2
+    ballY = VIRTUAL_HEIGHT / 2 - 2
+
+    ballDX = math.random(2) == 1 and 100 or -100
+    ballDY = math.random(-50, 50)
+
+    gameState = 'start'
+end
+
+function love.update(dt)
+
+    if love.keyboard.isDown('w') then
+        player1Y = math.max(0, player1Y + -PADDLE_SPEED * dt)
+    elseif love.keyboard.isDown('s') then
+        player1Y = math.min(VIRTUAL_HEIGHT - 20, player1Y + PADDLE_SPEED * dt)
+    end
+
+    if love.keyboard.isDown('up') then
+        player2Y = math.max(0, player2Y + -PADDLE_SPEED * dt)
+    elseif love.keyboard.isDown('down') then
+        player2Y = math.min(VIRTUAL_HEIGHT - 20, player2Y + PADDLE_SPEED * dt)
+    end
+
+    if gameState == 'play' then
+        ballX = ballX + ballDX * dt
+        ballY = ballY + ballDY * dt
+    end
+end
+
+function love.keypressed(key)
+    if key == 'escape' then
+        love.event.quit()
+   
+    elseif key == 'enter' or key == 'return' then
+        if gameState == 'start' then
+            gameState = 'play'
+        else
+            gameState = 'start'
+            
+            ballX = VIRTUAL_WIDTH / 2 - 2
+            ballY = VIRTUAL_HEIGHT / 2 - 2
+
+            ballDX = math.random(2) == 1 and 100 or -100
+            ballDY = math.random(-50, 50) * 1.5
+        end
+    end
+end
+
+
+function love.draw()
+    push:apply('start')
+
+    love.graphics.clear(43/255, 27/255, 23/255, 1)
+
+    if gameState == 'start' then
+        love.graphics.printf('Start State!', 0, 20, VIRTUAL_WIDTH, 'center')
+    else
+        love.graphics.printf('Play State!', 0, 20, VIRTUAL_WIDTH, 'center')
+    end
+
+    love.graphics.rectangle('fill', 15, player1Y, 4, 30)
+
+    love.graphics.rectangle('fill', VIRTUAL_WIDTH - 15, player2Y, 4, 30)
+
+    love.graphics.rectangle('fill', ballX, ballY, 4, 4)
+
+    push:apply('end')
+end
